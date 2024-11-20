b167a507c5ab7d6fb532c8693016c03f2c876fec
githubWex <github@we-x.io>
2024-11-20 11:45:18 -0600

    refactor: limpiar y mejorar la función main, agregar función para obtener la primera palabra

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,3 @@
-
-
 // fn variables(){
 //     let mut s: String = String::from("hola");
 //     s.push_str(", mundo");
@@ -35,10 +33,10 @@
 //     un_string
 // }
 
-//  
+//
 
 // fn main() {
-    
+
 //     let s1 = String::from("Hola");
 //     let len = calcular_longitud_ref(&s1);
 //     print!("La longitud de {s1 }es {len}");
@@ -55,7 +53,7 @@
 //     let r1 = &s; // no hay problema
 //     let r2 = &s; // no hay problema
 //     println!("{r1} y {r2}");
-    // variables r1 y r2 no se usaran más a partir de aquí
+// variables r1 y r2 no se usaran más a partir de aquí
 
 //     let r3 = &mut s; // no hay problema
 //     println!("{r3}");
@@ -64,7 +62,6 @@
 // fn main(){
 //     let referencia_a_la_nada = colgar();
 
-    
 // }
 
 // fn colgar() -> &String {
@@ -82,27 +79,57 @@
 //     s
 // }
 
-
 // El tipo de Slice
 
+// fn main() {
+//     let s: String = String::from("hola mundo");
 
+//     let palabra: &str = first_word(&s);
 
-fn main(){
-    let s = String::from("hola mundo");
+//     println!("{palabra}");
+// }
 
-    let palabra = first_word(&s);
+// fn first_word(s: &String) -> &str {
+//     let bytes = s.as_bytes();
 
-    println!("{palabra}");
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return &s[0..i];
+//         }
+//     }
+
+//     &s[..]
+// }
+
+//! String literales como Slice
+fn main() {
+    let my_string = String::from("hello world");
+    let word = first_word(&my_string[0..6]);
+    print!("{word}");
+    let word = first_word(&my_string[..]);
+
+    print!("{word}");
+
+    let my_string_literal = "hello world";
+
+    //* first_word funciona con slices de string literales, sean parciales o completos */
+    let word = first_word(&my_string_literal[0..6]);
+    print!("{word}");
+    let word = first_word(&my_string_literal[..]);
+    print!("{word}");
+
+    let word = first_word(my_string_literal);
+    print!("{word}");
 }
 
-fn first_word(s: &String) -> usize{
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
-    for (i, &item) in bytes.iter().enumerate(){
-        if item == b' '{
-            return i;
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
