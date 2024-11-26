4e329d18d7c42d7ce9902c9d8d0a163a314c9e43
githubWex <github@we-x.io>
2024-11-25 18:15:19 -0600

    chore: agregar archivos de bloqueo y productos de trabajo generados por el compilador

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,9 +1,9 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
+// struct User {
+//     active: bool,
+//     username: String,
+//     email: String,
+//     sign_in_count: u64,
+// }
 
 // fn main() {
 //     let mut user1 = User {
@@ -17,14 +17,14 @@ struct User {
 
 //     //*Esto crea otra instancia usando los valores de otra instancia */
 //     let user2 = User {
-       
+
 //         email: String::from("correo2@correo.com"),
 //         ..user1
 //     };
-    
+
 // }
 
-// //* Cuando tienes la misma variable y el mismo nombre de campo, puedes abreviarlo*/ 
+// //* Cuando tienes la misma variable y el mismo nombre de campo, puedes abreviarlo*/
 // fn build_user(email:String, username: String) -> User{
 //     User{
 //         active: true,
@@ -34,8 +34,7 @@ struct User {
 //     }
 // }
 
-
-// // //! Struct de Tuplas //
+// //! Struct de Tuplas //
 
 // struct Color( i32, i32, i32);
 // struct Point(i32, i32, i32);
@@ -45,23 +44,111 @@ struct User {
 //     let origin = Point(0, 0, 0);
 // }
 
-
 // //! Struct de unidad sin campos //
 
 // struct AlwaysActive;
-
 
 // fn main() {
 //     let _always_active = AlwaysActive;
 // }
 
+// fn main() {
+//     let user1: User = User {
+//         active: true,
+//         username: "AlgunUsuario".to_string(),
+//         email: "correo@corre.com".to_string(),
+//         sign_in_count: 1,
+//     };
+//     print!("{userr1}", userr1 = user1.username);
+// }
+
+// //* Programa de ejemplo usando struct */
+// //! Primera refactorizacion //
+
+// fn main() {
+//   let react1: (u32, u32) = (30, 50);
+
+//   panic!("El area del rectangulo es: \n{}", area(react1));
+// }
+
+// fn area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
+
+// //! Segunda refactorizacion //
+// #[derive(Debug)]
+// struct Reactangulo {
+//     width: u32,
+//     height: u32,
+// }
+// fn main() {
+//     let react1: Reactangulo = Reactangulo {
+//         width: 30,
+//         height: 50,
+//     };
+
+//     println!("El area del rectangulo es: \n{}", area(&react1));
+//     println!("El rectangulo es: \n {:#?}", react1);
+//     dbg!(react1);
+// }
+
+// fn area(rectangle: &Reactangulo) -> u32 {
+//     rectangle.width * rectangle.height
+// }
+
+// //? Sintaxis de los metodos //
+
+#[derive(Debug)]
+struct Reactangulo {
+    width: u32,
+    height: u32,
+}
+
+impl Reactangulo {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+impl Reactangulo {
+    fn can_hold(&self, other: &Reactangulo) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+impl Reactangulo {
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+}
 
 fn main() {
-    let user1 = User{
-        active: true,
-        username: "AlgunUsuario".to_string(),
-        email: "correo@corre.com".to_string(),
-        sign_in_count: 1,
+    let rect1: Reactangulo = Reactangulo {
+        width: 30,
+        height: 50,
     };
-    print!("{:?}", user1);
+
+    let rect2: Reactangulo = Reactangulo {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3: Reactangulo = Reactangulo {
+        width: 60,
+        height: 45,
+    };
+
+    let square: Reactangulo = Reactangulo::square(20);
+
+    println!("El area del rectangulo es: \n{}", rect1.can_hold(&rect2));
+    println!("El area del rectangulo es: \n{}", rect1.can_hold(&rect3));
+
+    println!("{:#?}", square);
 }
