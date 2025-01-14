ceab423d3d7a662f0c8eef8c8199b48e2666af7f
githubWex <github@we-x.io>
2025-01-14 09:41:50 -0600

    feat: agregar archivos de dependencia generados para los módulos rand y restaurant

---
+++ 07.paquetes-crates-modulos/target/debug/deps/restaurant-4e513776440370b6.d
@@ -0,0 +1,5 @@
+C:\Users\vladm\OneDrive\Escritorio\Rust\07.paquetes-crates-modulos\target\debug\deps\librestaurant-4e513776440370b6.rmeta: src\lib.rs
+
+C:\Users\vladm\OneDrive\Escritorio\Rust\07.paquetes-crates-modulos\target\debug\deps\restaurant-4e513776440370b6.d: src\lib.rs
+
+src\lib.rs:
