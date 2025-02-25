b1c2e15bce9ed89e9974ea00c6d06cacd9f89875
githubWex <github@we-x.io>
2025-02-25 11:04:40 -0600

    se crean nuevos ejercicios para el historial del proyecto

---
+++ ownership/target/debug/deps/ownership-373eafa17d9d3955.d
@@ -0,0 +1,5 @@
+C:\Users\vladm\OneDrive\Escritorio\Rust\ownership\target\debug\deps\libownership-373eafa17d9d3955.rmeta: src\main.rs
+
+C:\Users\vladm\OneDrive\Escritorio\Rust\ownership\target\debug\deps\ownership-373eafa17d9d3955.d: src\main.rs
+
+src\main.rs:
