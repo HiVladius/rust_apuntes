026e6036b6ea76e0bb03ee61756f5cde34ec5cda
vladius <vladpsico@gmail.com>
2025-04-21 16:59:20 -0600

    Desarrollo de migrep para practica de desarrollo del lenguaje

---
+++ 12.minigrep/target/debug/deps/minigrep-c36bec4d7e8a8d7c.d
@@ -0,0 +1,5 @@
+C:\Users\vladm\OneDrive\Escritorio\Rust\minigrep\target\debug\deps\minigrep-c36bec4d7e8a8d7c.exe: src\lib.rs
+
+C:\Users\vladm\OneDrive\Escritorio\Rust\minigrep\target\debug\deps\minigrep-c36bec4d7e8a8d7c.d: src\lib.rs
+
+src\lib.rs:
